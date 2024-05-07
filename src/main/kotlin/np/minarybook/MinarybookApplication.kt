9d710a4045e96340088eecdbf7d87774865c7e7c
trust1204@gmail.com <trust1204@gmail.com>
2024-05-07 19:55:50 +0900

    first commit

---
+++ src/main/kotlin/np/minarybook/MinarybookApplication.kt
@@ -0,0 +1,11 @@
+package np.minarybook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinarybookApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinarybookApplication>(*args)
+}
---
+++ src/test/kotlin/np/minarybook/MinarybookApplicationTests.kt
@@ -0,0 +1,13 @@
+package np.minarybook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinarybookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
