53149ecc18ef832953ab3208005697179f0bc924
Seungkyu-Han <trust1204@gmail.com>
2024-05-22 17:13:01 +0900

    feat: BookForRent Get List API

--- src/main/kotlin/np/minarybook/application/BookForRentService.kt
+++ src/main/kotlin/np/minarybook/application/BookForRentService.kt
@@ -2,7 +2,9 @@ package np.minarybook.application
 
 import np.minarybook.model.dto.bookForRent.req.BookForRentPostReq
 import np.minarybook.model.dto.bookForRent.req.BookForRentPutReq
+import np.minarybook.model.dto.bookForRent.res.BookForRentGetElementRes
 import np.minarybook.model.dto.bookForRent.res.BookForRentGetRes
+import np.minarybook.model.enum.Category
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
@@ -13,4 +15,5 @@ interface BookForRentService {
     fun delete(id: Int, authentication: Authentication): ResponseEntity<HttpStatus>
     fun put(bookForRentPutReq: BookForRentPutReq, authentication: Authentication): ResponseEntity<HttpStatus>
     fun patchSold(bookForRentId: Int, authentication: Authentication): ResponseEntity<HttpStatus>
+    fun getList(category: Category?, authentication: Authentication): ResponseEntity<List<BookForRentGetElementRes>>
 }
--- src/main/kotlin/np/minarybook/repository/BookForRentRepository.kt
+++ src/main/kotlin/np/minarybook/repository/BookForRentRepository.kt
@@ -2,8 +2,14 @@ package np.minarybook.repository
 
 import np.minarybook.model.entity.BookForRent
 import np.minarybook.model.entity.User
+import np.minarybook.model.enum.Category
+import np.minarybook.model.enum.State
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BookForRentRepository: JpaRepository<BookForRent, Int> {
+    fun findByCategoryAndStateOrderByIdDesc(category: Category, state: State, pageable: Pageable): List<BookForRent>
+
     fun deleteByIdAndUser(id: Int, user: User)
+    fun findByStateOrderByIdDesc(sale: State, pageable: Pageable): List<BookForRent>
 }
