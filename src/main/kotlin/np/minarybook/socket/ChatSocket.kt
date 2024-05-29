2f70ba46b35a8d802e2e990b6cee8fa4469cb0e8
Seungkyu-Han <trust1204@gmail.com>
2024-05-29 17:35:25 +0900

    fix: Socket ERROR

--- src/main/kotlin/np/minarybook/socket/ChatSocket.kt
+++ src/main/kotlin/np/minarybook/socket/ChatSocket.kt
@@ -12,12 +12,15 @@ import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 import java.io.IOException
 import java.lang.RuntimeException
+import java.util.concurrent.CopyOnWriteArrayList
 
 @Component
 class ChatSocket(private val chatRepository: ChatRepository,
-private val objectMapper: ObjectMapper, private val jwtTokenProvider: JwtTokenProvider):TextWebSocketHandler() {
-    private val chatWebSocketHandler: MutableList<ChatWebSocketSession> = ArrayList()
-    private val webSocketHandler: MutableList<WebSocketSession> = ArrayList()
+                 private val objectMapper: ObjectMapper,
+                 private val jwtTokenProvider: JwtTokenProvider) : TextWebSocketHandler() {
+
+    private val chatWebSocketHandler: MutableList<ChatWebSocketSession> = CopyOnWriteArrayList()
+    private val webSocketHandler: MutableList<WebSocketSession> = CopyOnWriteArrayList()
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
         webSocketHandler.add(session)
@@ -25,9 +28,12 @@ private val objectMapper: ObjectMapper, private val jwtTokenProvider: JwtTokenPr
 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
         webSocketHandler.remove(session)
-        chatWebSocketHandler.map {
-            chatWebSocketSession ->
-            if(chatWebSocketSession.webSocketSession == session) chatWebSocketHandler.remove(chatWebSocketSession)
+        val iterator = chatWebSocketHandler.iterator()
+        while (iterator.hasNext()) {
+            val chatWebSocketSession = iterator.next()
+            if (chatWebSocketSession.webSocketSession == session) {
+                iterator.remove()
+            }
         }
         super.afterConnectionClosed(session, status)
     }
@@ -35,28 +41,28 @@ private val objectMapper: ObjectMapper, private val jwtTokenProvider: JwtTokenPr
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         val payload = message.payload
         val isEstablished = objectMapper.readTree(payload).get("isEstablished").asBoolean()
-        if(isEstablished){
+        if (isEstablished) {
             val sender = objectMapper.readTree(payload).get("accessToken").asText()
             val receiver = objectMapper.readTree(payload).get("receiver").asLong()
             chatWebSocketHandler.add(ChatWebSocketSession(session, User(jwtTokenProvider.getId(sender) ?: 0), User(receiver)))
-        }
-        else{
+        } else {
             val sender = objectMapper.readTree(payload).get("accessToken").asText()
-            val senderId = jwtTokenProvider.getId(sender)?:0
+            val senderId = jwtTokenProvider.getId(sender) ?: 0
             val receiver = objectMapper.readTree(payload).get("receiver").asLong()
             val content = objectMapper.readTree(payload).get("content").asText()
             println(senderId)
             println(receiver)
             println(content)
             chatRepository.save(Chat(User(senderId), User(receiver), content))
-            for(chatWebSocketSession in chatWebSocketHandler){
+            for (chatWebSocketSession in chatWebSocketHandler) {
                 println("receiver: ${chatWebSocketSession.receiver.id}, sender: ${chatWebSocketSession.sender.id}")
-                if(chatWebSocketSession.receiver.id == receiver && chatWebSocketSession.sender.id == senderId){
-                    try{
+                if (chatWebSocketSession.receiver.id == receiver && chatWebSocketSession.sender.id == senderId) {
+                    try {
+                        println("SEND MESSAGE TO $receiver FROM $senderId CONTENT${content}")
                         chatWebSocketSession.webSocketSession.sendMessage(TextMessage(
                             objectMapper.writeValueAsBytes(content)
                         ))
-                    }catch (e: IOException){
+                    } catch (e: IOException) {
                         throw RuntimeException(e)
                     }
                 }
